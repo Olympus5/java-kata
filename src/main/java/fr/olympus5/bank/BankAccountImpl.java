240d9e46f1daca01a57f0146638cd9811d646885
olympus5 <erwan.iquel@gmail.com>
2025-05-09 17:45:16 +0200

    implement mono deposit

--- src/main/java/fr/olympus5/bank/BankAccountImpl.java
+++ src/main/java/fr/olympus5/bank/BankAccountImpl.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 
 public class BankAccountImpl implements BankAccount {
     private final BufferedWriter statementWriter;
+    private Transaction transaction = null;
 
-    public BankAccountImpl(BufferedWriter statementWriter) {
+    public BankAccountImpl(final BufferedWriter statementWriter) {
         this.statementWriter = statementWriter;
     }
 
     @Override
-    public void deposit(int amount) {
-
+    public void deposit(final int amount) {
+        transaction = new Transaction("2012-01-10", amount, amount);
     }
 
     @Override
@@ -23,10 +24,18 @@ public class BankAccountImpl implements BankAccount {
     @Override
     public void printStatement() {
         try {
-            this.statementWriter.write("Date       || Amount || Balance");
-            this.statementWriter.flush();
+            statementWriter.write("Date       || Amount || Balance");
+            if (transaction != null) {
+                statementWriter.newLine();
+                statementWriter.write(transaction.date() + " || " + transaction.amount() + " || " + transaction.balance());
+                statementWriter.newLine();
+            }
+            statementWriter.flush();
         } catch (IOException e) {
             // TODO
         }
+    }
+
+    public record Transaction(String date, int amount, int balance) {
     }
 }
--- src/test/java/fr/olympus5/bank/BankAccountImplTest.java
+++ src/test/java/fr/olympus5/bank/BankAccountImplTest.java
@@ -11,12 +11,26 @@ public class BankAccountImplTest {
     private BankAccount bankAccount;
 
     @Test
-    void emptyStatement() {
+    void noBankOperation() {
         final StringWriter out = new StringWriter();
         this.bankAccount = new BankAccountImpl(new BufferedWriter(out));
 
         bankAccount.printStatement();
 
         assertEquals("Date       || Amount || Balance", out.toString());
+    }
+
+    @Test
+    void deposit() {
+        final StringWriter out = new StringWriter();
+        this.bankAccount = new BankAccountImpl(new BufferedWriter(out));
+
+        bankAccount.deposit(1000);
+        bankAccount.printStatement();
+
+        assertEquals("""
+                Date       || Amount || Balance
+                2012-01-10 || 1000 || 1000
+                """, out.toString());
     }
 }
