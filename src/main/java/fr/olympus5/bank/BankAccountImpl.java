617ad8208066c2313f2c04a2f64baf17c99d72e5
olympus5 <erwan.iquel@gmail.com>
2025-05-09 23:10:40 +0200

    refactor 'BankAccountImpl' class

--- src/main/java/fr/olympus5/bank/BankAccountImpl.java
+++ src/main/java/fr/olympus5/bank/BankAccountImpl.java
@@ -4,16 +4,18 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class BankAccountImpl implements BankAccount {
+    private final TransactionRepository transactionRepository;
     private final BufferedWriter statementWriter;
     private Transaction transaction = null;
 
-    public BankAccountImpl(final BufferedWriter statementWriter, TransactionRepository transactionRepository) {
+    public BankAccountImpl(final BufferedWriter statementWriter, final TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
         this.statementWriter = statementWriter;
     }
 
     @Override
     public void deposit(final int amount) {
-        transaction = new Transaction("2012-01-10", amount, amount);
+        transactionRepository.save(new Transaction("2012-01-10", amount, amount));
     }
 
     @Override
@@ -25,11 +27,11 @@ public class BankAccountImpl implements BankAccount {
     public void printStatement() {
         try {
             statementWriter.write("Date       || Amount || Balance");
-            if (transaction != null) {
-                statementWriter.newLine();
-                statementWriter.write(transaction.date() + " || " + transaction.amount() + " || " + transaction.balance());
-                statementWriter.newLine();
-            }
+            if(!transactionRepository.findAll().isEmpty()) statementWriter.newLine();
+            final String rows = transactionRepository.findAll().stream()
+                    .map(tx -> String.format("%s || %s || %s%n", tx.date(), tx.amount(), tx.balance()))
+                    .reduce(String::concat).orElse("");
+            statementWriter.write(rows);
             statementWriter.flush();
         } catch (IOException e) {
             // TODO
