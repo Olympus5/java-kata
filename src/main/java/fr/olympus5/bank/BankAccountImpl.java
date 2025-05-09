fae12379bca0afa1899095e2a0b76a8921159df0
olympus5 <erwan.iquel@gmail.com>
2025-05-09 16:56:58 +0200

    add statement header

--- src/main/java/fr/olympus5/bank/BankAccountImpl.java
+++ src/main/java/fr/olympus5/bank/BankAccountImpl.java
@@ -1,6 +1,15 @@
 package fr.olympus5.bank;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class BankAccountImpl implements BankAccount {
+    private final BufferedWriter statementWriter;
+
+    public BankAccountImpl(BufferedWriter statementWriter) {
+        this.statementWriter = statementWriter;
+    }
+
     @Override
     public void deposit(int amount) {
 
@@ -13,6 +22,11 @@ public class BankAccountImpl implements BankAccount {
 
     @Override
     public void printStatement() {
-
+        try {
+            this.statementWriter.write("Date       || Amount || Balance");
+            this.statementWriter.flush();
+        } catch (IOException e) {
+            // TODO
+        }
     }
 }
--- src/test/java/fr/olympus5/bank/BankAccountImplTest.java
+++ src/test/java/fr/olympus5/bank/BankAccountImplTest.java
@@ -2,11 +2,21 @@ package fr.olympus5.bank;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.StringWriter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankAccountImplTest {
     private BankAccount bankAccount;
 
     @Test
     void emptyStatement() {
-        this.bankAccount = new BankAccountImpl();
+        final StringWriter out = new StringWriter();
+        this.bankAccount = new BankAccountImpl(new BufferedWriter(out));
+
+        bankAccount.printStatement();
+
+        assertEquals("Date       || Amount || Balance", out.toString());
     }
 }
